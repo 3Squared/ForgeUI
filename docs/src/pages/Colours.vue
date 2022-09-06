4378aed8dc05770adb68127b5ca3bbb8c2e2be49
Jacob Minor <jacob.minor@3squared.com>
2022-09-06 11:10:14 +0100

    Update colours page to include colour picker

--- docs/src/pages/Colours.vue
+++ docs/src/pages/Colours.vue
@@ -1,81 +1,49 @@
 ﻿<template>
-  <div class="position-absolute w-100 colours">
-    <h1>Colours</h1>
-    <div class="pl-5">
-      <h3 class="mt-4">- Brand Colours:</h3>
-      <div class="color-swatch">
-        <div class="bg-brand-shade-1"></div>
-        <div class="bg-brand-shade-2"></div>
-        <div class="bg-brand-shade-3"></div>
-        <div class="bg-brand"></div>
-        <div class="bg-brand-dark"></div>
-      </div>
-  
-      <h3 class="mt-4">- Greys:</h3>
-      
-      <div class="color-swatch">
-        <div class="bg-grey-100"></div>
-        <div class="bg-grey-200"></div>
-        <div class="bg-grey-300"></div>
-        <div class="bg-grey-400"></div>
-        <div class="bg-grey-500"></div>
-        <div class="bg-grey-600"></div>
-        <div class="bg-grey-700"></div>
-        <div class="bg-grey-800"></div>
-        <div class="bg-grey-900"></div>
-        <div class="bg-grey-1000"></div>
-      </div>
-  
-      <h3 class="mt-4">- Primary Colours:</h3>
-      <div class="color-swatch">
-        <div class="bg-primary-light"></div>
-        <div class="bg-primary"></div>
-        <div class="bg-primary-dark"></div>
-      </div>
-      
-      <h3 class="mt-4">- Success Colours:</h3>
-      <div class="color-swatch">
-        <div class="bg-success-light"></div>
-        <div class="bg-success"></div>
-        <div class="bg-success-dark"></div>
-      </div>
-  
-      <h3 class="mt-4">- Warning Colours:</h3>
-      <div class="color-swatch">
-        <div class="bg-warning-light"></div>
-        <div class="bg-warning"></div>
-        <div class="bg-warning-dark"></div>
-      </div>
-  
-      <h3 class="mt-4">- Danger Colours:</h3>
-      <div class="color-swatch">
-        <div class="bg-danger-light"></div>
-        <div class="bg-danger"></div>
-        <div class="bg-danger-dark"></div>
+  <div>
+    <div v-for="palette in colours" class="mb-5">
+      <h1 class="pb-3">{{ palette.title }}</h1>
+      <div class="d-flex color-swatch pb-5">
+        <div v-for="colour in palette.colours">
+          <div :id="`swatch-${colour}`" :class="colour"></div>
+          <b-dropdown variant="link" text="Copy" class="h-50">
+            <b-dropdown-item @click="getColour(colour)">Hex Code</b-dropdown-item>
+            <b-dropdown-item @click="copyToClipboard(colour)">Boostrap Class</b-dropdown-item>
+          </b-dropdown>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import { BDropdown, BDropdownItem } from "bootstrap-vue";
+import rgbHex from "rgb-hex";
 
-const d = ref('a')
+const colours = [
+  { title: 'Neutral Shades', colours: ['bg-grey-900', 'bg-grey-800', 'bg-grey-700', 'bg-grey-600', 'bg-grey-500', 'bg-grey-400', 'bg-grey-300', 'bg-grey-200', 'bg-grey-100'] },
+  { title: 'System Colours', colours: ['bg-primary', 'bg-secondary', 'bg-success', 'bg-success-light', 'bg-info', 'bg-warning', 'bg-warning-light', 'bg-danger', 'bg-light', 'bg-dark'] }
+]
+
+const copyToClipboard = (value: string) => {
+  navigator.clipboard.writeText(value)
+}
+
+const getColour = (colour: string) => {
+  const swatch = document.getElementById(`swatch-${colour}`) as Element
+  const swatchStyles = getComputedStyle(swatch)
+  const hex = `#${rgbHex(swatchStyles.backgroundColor)}`
+  copyToClipboard(hex)
+}
 </script>
 
 <style lang="scss">
 .color-swatch {
-  display: flex;
-  height: 50px;
-  padding-left: 15px;;
+  height: 64px;
 
   div {
-    height: 50px;
-    width: 50px;
+    margin-right: 15px;
+    height: 64px;
+    width: 64px;
   }
-}
-
-.colours {
-  left: 50px;
 }
 </style>
