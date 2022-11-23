f09cd39a015d773e09b7a6e26f5dfb61fad00467
Tom Smith <tom.smith2711@gmail.com>
2022-11-23 15:27:36 +0000

    fix tests

--- packages/ui/src/components/forms/multiselect-preview/MultiselectPreview.cy.ts
+++ packages/ui/src/components/forms/multiselect-preview/MultiselectPreview.cy.ts
@@ -1,10 +1,29 @@
-import { mount } from 'cypress/vue2';
-import ForgeMultiselectPreview from './MultiSelectPreview.vue';
+import { mount } from "cypress/vue2";
+import ForgeMultiselectPreview from "./MultiSelectPreview.vue";
+import { ref } from "vue";
 
-import { items, selectedItems } from 'apps/docs/src/pages/forms/multiselect-preview/examples/example-data'
+const items = [
+  { id: 1, label: "Andrew Powell (Senior)", labelShort: "Andrew Powell" },
+  { id: 2, label: "Tom Smith (Senior)", labelShort: "Tom Smith" },
+  { id: 3, label: "Richard Wilson (Dev)", labelShort: "Richard Wilson" },
+  { id: 4, label: "Josh Sexton-Jones (Junior Dev)", labelShort: "Josh Sexton-Jones" },
+  { id: 5, label: "Adam Thompson (Dev)", labelShort: "Adam Thompson" },
+  { id: 6, label: "Tom McGurrin (Senior)", labelShort: "Tom McGurrin" },
+  { id: 7, label: "Hamza Mahmood (Dev)", labelShort: "Hamza Mahmood" },
+  { id: 8, label: "Isabel Mallon (Junior Dev)", labelShort: "Isabel Mallon" }
+];
 
-describe('MultiSelectPreview.vue', () => {
-  it('renders correctly', () => {
+export { items };
+
+const selectedItems = ref([
+  { id: 1, label: "Andrew Powell (Senior)", labelShort: "Andrew Powell" },
+  { id: 2, label: "Tom Smith (Senior)", labelShort: "Tom Smith" },
+  { id: 7, label: "Hamza Mahmood (Dev)", labelShort: "Hamza Mahmood" },
+  { id: 8, label: "Isabel Mallon (Junior Dev)", labelShort: "Isabel Mallon" }
+]);
+
+describe("MultiSelectPreview.vue", () => {
+  it("renders correctly", () => {
     mount(ForgeMultiselectPreview, {
       propsData: {
         items: items,
@@ -13,9 +32,9 @@ describe('MultiSelectPreview.vue', () => {
       }
     });
     
-    cy.get('[data-cy=forge-multiselect-preview-1]').contains('Andrew Powell (Senior)')
-    cy.get('[data-cy=forge-multiselect-preview-1]').contains('Tom Smith (Senior)')
-    cy.get('[data-cy=forge-multiselect-preview-1]').contains('Hamza Mahmood (Dev)')
-    cy.get('[data-cy=forge-multiselect-preview-1]').contains('Isabel Mallon (Junior Dev)')
+    cy.get("[data-cy=forge-multiselect-preview-1]").contains("Andrew Powell (Senior)");
+    cy.get("[data-cy=forge-multiselect-preview-1]").contains("Tom Smith (Senior)");
+    cy.get("[data-cy=forge-multiselect-preview-1]").contains("Hamza Mahmood (Dev)");
+    cy.get("[data-cy=forge-multiselect-preview-1]").contains("Isabel Mallon (Junior Dev)");
   });
 });
--- packages/ui/src/components/general/action-button/ActionButton.cy.ts
+++ packages/ui/src/components/general/action-button/ActionButton.cy.ts
@@ -1,12 +1,29 @@
-﻿import { mount } from 'cypress/vue2'
+﻿import { mount } from "cypress/vue2";
 import ActionButton from "./ActionButton.vue";
 import { defineComponent } from "vue";
 
 
-it('should become disabled while action is resolving', () => {
+it("should become disabled while action is resolving", () => {
   mount(ActionButton, {
     slots: {
-      default: 'Some Text',
+      default: "Some Text",
+    },
+    propsData: { 
+      action: () => new Promise(resolve => setTimeout(resolve, 1000))
+    },
+    attrs: {
+      disabled: false
+    }
+  });
+  cy.get("button").should("not.have.attr", "disabled");
+  cy.get("button").click();
+  cy.get("button").should("have.attr", "disabled");
+});
+
+it("should reset to un disabled after an action", () => {
+  mount(ActionButton, {
+    slots: {
+      default: "Some Text",
     },
     propsData: {
       action: () => new Promise(resolve => setTimeout(resolve, 1000))
@@ -15,42 +32,25 @@ it('should become disabled while action is resolving', () => {
       disabled: false
     }
   });
-  cy.get('button').should('not.have.attr', 'disabled')
-  cy.get('button').click();
-  cy.get('button').should('have.attr', 'disabled')
+  cy.get("button").should("not.have.attr", "disabled");
+  cy.get("button").click();
+  cy.get("button").should("have.attr", "disabled");
+  cy.wait(1000);
+  cy.get("button").should("not.have.attr", "disabled");
 });
 
-it('should reset to un disabled after an action', () => {
-  mount(ActionButton, {
-    slots: {
-      default: 'Some Text',
-    },
-    propsData: {
-      action: () => new Promise(resolve => setTimeout(resolve, 1000))
-    },
-    attrs: {
-      disabled: false
-    }
-  });
-  cy.get('button').should('not.have.attr', 'disabled')
-  cy.get('button').click();
-  cy.get('button').should('have.attr', 'disabled')
-  cy.wait(1000)
-  cy.get('button').should('not.have.attr', 'disabled')
-});
-
-it('should reset to un disabled after an action', () => {
+it("should reset to un disabled after an action", () => {
   const wrapper = defineComponent({
     components: { ActionButton },
     data() {
       return {
         isButtonDisabled: false
-      }
+      };
     },
     methods: {
       exampleSave() {
         this.isButtonDisabled = true;
-        return new Promise(resolve => setTimeout(resolve, 1000))
+        return new Promise(resolve => setTimeout(resolve, 1000));
       }
     },
     template: `
@@ -58,10 +58,10 @@ it('should reset to un disabled after an action', () => {
       <ActionButton :disabled="isButtonDisabled" :action="exampleSave">Test button</ActionButton>
       </div>`
   });
-  mount(wrapper)
-  cy.get('button').should('not.have.attr', 'disabled')
-  cy.get('button').click();
-  cy.get('button').should('have.attr', 'disabled')
-  cy.wait(1000)
-  cy.get('button').should('have.attr', 'disabled')
+  mount(wrapper);
+  cy.get("button").should("not.have.attr", "disabled");
+  cy.get("button").click();
+  cy.get("button").should("have.attr", "disabled");
+  cy.wait(1000);
+  cy.get("button").should("have.attr", "disabled");
 });
